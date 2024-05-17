eca37524cdc93ef337bbde2dd99ed916632c7927
Rafael Borges Dantas <rafael.dantas@nerus.com.br>
2024-05-17 09:39:34 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,22 @@
+const colors = ["green", "red", "rgba(133,122,200)"];
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
+const voltar = document.getElementById("btnIndex")
+
+btn.addEventListener("click", function () {
+  const randomNumber = getRandomNumber();
+   //console.log(randomNumber);
+
+  document.body.style.backgroundColor = colors[randomNumber];
+  color.textContent = colors[randomNumber];
+});
+
+voltar.addEventListener("click", function () {
+
+  //alert("teste");
+  this.addEventListener = window.history.back();
+});
+
+function getRandomNumber() {
+  return Math.floor(Math.random() * colors.length);
+}
---
+++ hex.js
@@ -0,0 +1,24 @@
+const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
+const voltar = document.getElementById("btnVoltar");
+
+btn.addEventListener("click", function () {
+  let hexColor = "#";
+  for (let i = 0; i < 6; i++) {
+    hexColor += hex[getRandomNumber()];
+  }
+  // console.log(hexColor);
+
+  color.textContent = hexColor;
+  document.body.style.backgroundColor = hexColor;
+});
+
+voltar.addEventListener("click", function() {
+  //alert ("teste");
+    this.addEventListener = window.history.back();
+});
+
+function getRandomNumber() {
+  return Math.floor(Math.random() * hex.length);
+}
